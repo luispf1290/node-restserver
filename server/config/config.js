a38ff6af6cc46dcf304bcc9637c4ea797c05de7b
luis piña <luispia1290@gmail.com>
2018-07-30 15:19:30 -0500

    RestServer- cambio de config

--- server/config/config.js
+++ server/config/config.js
@@ -19,11 +19,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') {
-	//urlDB = 'mongodb://localhost:27017/cafe';
-//} else {
-urlDB = 'mongodb://cafe-user:abcd12345@ds159651.mlab.com:59651/cafe';
-//}
+if (process.env.NODE_ENV === 'dev') {
+	urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+	urlDB = 'mongodb://cafe-user:abcd12345@ds159651.mlab.com:59651/cafe';
+}
 
 
 process.env.URLDB = urlDB;
